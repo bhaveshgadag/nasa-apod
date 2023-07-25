5e441e1a35c48364519bbbeecfe1fd3c4e4a1023
bhaveshhadag <bhaveshgadag46@gmail.com>
2023-07-25 07:34:14 +0530

    get latest image on page load

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from nasa_api import get_apod
+import requests
 import streamlit as st
 
 st.title("hello")
 
+content = get_apod()
+
+image = requests.get(content['url'])
+
+with open('image.jpg', 'wb') as file:
+    file.write(image.content)
+
+print(content)
+st.image()
