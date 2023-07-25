8d7bcc00ae3fb98c127b9ab23655c83be1d7d233
bhaveshhadag <bhaveshgadag46@gmail.com>
2023-07-25 08:55:16 +0530

    Add title, image and explanation

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from nasa_api import get_apod
 import requests
 import streamlit as st
 
-st.title("hello")
-
 content = get_apod()
 
 image = requests.get(content['url'])
@@ -11,5 +9,6 @@ image = requests.get(content['url'])
 with open('image.jpg', 'wb') as file:
     file.write(image.content)
 
-print(content)
-st.image()
+st.title(content['title'])
+st.image("image.jpg")
+st.write(content['explanation'])
